ea2a99ff3eaa30f84a922bd150c97447ec8a68cb
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-05-11 19:47:12 +0200

    [test2] added output file

--- scripts/resolver_example.py
+++ scripts/resolver_example.py
@@ -8,16 +8,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 
 if __name__ == "__main__":
-    METAMODELS = "https://example.com/metamodels/entities/"
-    MODELS = "https://example.com/instances/"
+    METAMODELS = "https://example.com/point#"
+    MODELS = "https://example.com/rob#"
 
     url_map = {
         METAMODELS: "metamodels/entities/",
-        MODELS: "instances/"
+        MODELS: "models/"
     }
     resolver.install(resolver.IriToFileResolver(url_map))
 
-    g = rdflib.Graph()
-    g.parse(MODELS + "point_instance.jsonld", format="json-ld")
+    g = rdflib.ConjunctiveGraph()
+    g.parse("models/points.jsonld", format="json-ld")
 
     print(g.serialize(format="turtle"))
