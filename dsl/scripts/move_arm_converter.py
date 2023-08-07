c3596708118e15107692a76dd37226931133f0c3
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-08-07 22:15:26 +0200

    added wrench spec support and bug fixes

--- dsl/scripts/move_arm_converter.py
+++ dsl/scripts/move_arm_converter.py
@@ -63,7 +63,7 @@ class Convert:
         if not self.no_render:
 
             template = self.templates_env.get_template(
-            'src/move_arm_template.cpp.jinja2')
+                'src/move_arm_template.cpp.jinja2')
 
             ns = "http://example.com/rob#"
 
@@ -104,32 +104,34 @@ class Convert:
             elif coord_data['type'] == 'VelocityCoordinate':
                 of_coord_data = self.query_utils.get_coord_info(
                     coord_data['of_coord'])
-                
+
                 wrt_coord_data = self.query_utils.get_coord_info(
                     coord_data['wrt_coord'])
-                
+
                 asb_coord_data = self.query_utils.get_coord_info(
                     coord_data['asb_coord'])
-                
+
                 big_data['coords'][coord_data['of_coord']] = of_coord_data
                 big_data['coords'][coord_data['wrt_coord']] = wrt_coord_data
                 big_data['coords'][coord_data['asb_coord']] = asb_coord_data
 
                 big_data['coords'][coord_data['of_coord']+'_twist'] = {
                     'type': ['TwistCoordinate']}
-                
-            elif coord_data['type'] == 'ForceCoordinate':
-                if coord_data['ab_coord'].replace(ns, '') not in big_data['coords']:
+
+            elif coord_data['type'] == 'WrenchCoordinate':
+                if coord_data['ab_coord'].replace(
+                        ns, '') not in big_data['coords']:
                     ab_coord_data = self.query_utils.get_coord_info(
                         coord_data['ab_coord'])
                     big_data['coords'][coord_data['ab_coord']] = ab_coord_data
-                
+
                 # if coord_data['at_coord'].replace(ns, '') not in big_data['coords']:
                 #     at_coord_data = self.query_utils.get_coord_info(
                 #         coord_data['at_coord'])
                 #     big_data['coords'][coord_data['at_coord']] = at_coord_data
-                
-                if coord_data['asb_coord'].replace(ns, '') not in big_data['coords']:
+
+                if coord_data['asb_coord'].replace(
+                        ns, '') not in big_data['coords']:
                     asb_coord_data = self.query_utils.get_coord_info(
                         coord_data['asb_coord'])
                     big_data['coords'][coord_data['asb_coord']] = asb_coord_data
@@ -194,13 +196,13 @@ class Convert:
                         motion_spec_iri, MOTIONSPEC["per-conditions"])
                     post_conditions = g.objects(
                         motion_spec_iri, MOTIONSPEC["post-conditions"])
-                    
+
                     pre_conditions_d = []
                     for pre_condition in pre_conditions:
-                        
+
                         pcr, ci = self.query_utils.get_pre_post_condition_info(
                             pre_condition, {'constraint': pre_condition})
-                        
+
                         big_data["constraints"][pcr['constraint']
                                                 ] = ci[pcr['constraint']]
                         big_data = self._update_coords_data(
@@ -228,20 +230,21 @@ class Convert:
                         pcr, ci = self.query_utils.get_per_condition_info(
                             per_condition)
                         per_conditions_d.append(pcr)
-                        
+
                         big_data["constraints"][pcr['constraint']
                                                 ] = ci[pcr['constraint']]
-                        
+
                         big_data = self._update_coords_data(
                             ci[pcr['constraint']], big_data)
-                        
+
                         if pcr['type'] != 'ForceConstraint':
                             cont_info = self.query_utils.get_pid_controller_info(
                                 pcr['controller'])
-                            big_data["controllers"][pcr['controller']] = cont_info
+                            big_data["controllers"][
+                                pcr['controller']] = cont_info
 
                         alpha = self.query_utils.get_alpha(pcr['constraint'])
-                        
+
                         if alpha is None:
                             print(f"Alpha is None for {pcr['constraint']}")
                         elif isinstance(alpha[0], list):
@@ -250,9 +253,9 @@ class Convert:
                         elif isinstance(alpha[0], float):
                             alphas.append(alpha)
                         else:
-                           raise Exception(
-                                f"Unknown alpha type {type(alpha)}") 
-                            
+                            raise Exception(
+                                f"Unknown alpha type {type(alpha)}")
+
                     motion_spec['per_conditions'] = per_conditions_d
 
                     # construct alpha, beta and nc
@@ -265,7 +268,7 @@ class Convert:
                     }
 
                     motion_specs[str(motion_spec_iri).replace(
-                        ROB + "/rob#", '')] = motion_spec     
+                        ROB + "/rob#", '')] = motion_spec
                 else:
                     raise Exception(
                         f"Motion specification {motion_spec_iri} is not supported")
